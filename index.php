16be11216b3fd49dd35ae7f01c2f0cf367d4445b
Felipe <santinha_sarinhax23@outlook.com>
2022-04-28 09:23:26 -0400

    iniciando projeto 3

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php 
 
-require_once("vendor/autoload.php");
+session_start();
 
-$app = new \Slim\Slim();
+require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \felipeno22\Page;
+use \felipeno22\PageAdmin;
+use \felipeno22\Model\User;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
@@ -10,13 +16,65 @@ $app->get('/', function() {
     
 	//echo "OK";
 	
-	$sql= new felipeno22\DB\Sql();
+/*	$sql= new felipeno22\DB\Sql();
 	
 	$results= $sql->select("select * from tb_users");
 	echo json_encode($results);
+	*/
 	
+	$page= new Page();
+	
+	$page->setTpl("index");
 
 });
+
+
+
+
+$app->get('/admin', function() {
+    //verificando sessao do login
+   User::verifyLogin();
+	
+	$page_admin= new PageAdmin();
+	
+	$page_admin->setTpl("index");
+
+});
+
+
+$app->get('/admin/login', function() {
+    
+	$page_admin = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page_admin->setTpl("login");
+
+});
+
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST['deslogin'], $_POST['despassword']);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+
+$app->get('/admin/logout', function() {
+
+	
+	User::logout();
+	
+
+	header("Location: /admin/login");
+	exit;
+
+});
+
 
 $app->run();
 
---
+++ vendor/felipeno22/php-classes/src/Page.php
@@ -0,0 +1,92 @@
+<?php 
+
+namespace felipeno22;
+
+use Rain\Tpl;
+
+class Page{
+	
+		private $tpl;
+		private $defaults=[
+				"header"=>true,
+				"footer"=>true,
+				"data"=>[]];
+		private $options=[];
+	
+	public function __construct($opts= array(),$tpl_dir="/views/"){
+		
+		$this->options=array_merge($this->defaults,$opts);
+		
+		$config=array(
+		"tpl_dir"=>$_SERVER['DOCUMENT_ROOT'].$tpl_dir,
+		"cache_dir"=>$_SERVER['DOCUMENT_ROOT']."/cache-views/",
+		 "debug"=> false
+
+	);
+
+
+	Tpl::configure($config);
+	$this->tpl=new Tpl();
+	
+	
+	
+	if ($this->options['data']){
+		$this->setData($this->options['data']);
+	} 
+		
+
+		if ($this->options['header'] === true){
+			$this->tpl->draw("header");
+		} 
+		
+	}
+	
+	
+	
+	
+
+	private function setData($data = array())
+	{
+
+		foreach ($data as $key => $value) {
+			$this->tpl->assign($key, $value);
+		}
+
+	}
+
+
+	
+
+	public function setTpl($name,$data=array(),$returnHtml=false){
+
+
+			$this->setData($data);
+
+		
+
+			return $this->tpl->draw($name,$returnHtml);
+
+
+
+
+
+	}
+	
+	public function __destruct(){
+		
+			if ($this->options['footer'] === true){
+				
+					$this->tpl->draw("footer");
+					
+			} 
+		
+	}
+	
+	
+	
+}
+
+
+
+
+?>
